4d0776dcf26eec9ac79d615c7239809dd83f6299
Dvimin <dmitriev.michail@mail.ru>
2023-11-19 16:36:29 +0300

    class

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <map>
 
 class Wrapper{
 private:
@@ -6,7 +9,25 @@ private:
 public:
 
 };
+class Subject{
+private:
+    std::map <std::string, int> mp;
+    //std::map <std::string, int>::iterator <mp>;
+
+public:
+
+    void f3(){
+
+    }
+};
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+//    Wrapper wrappwer(&subj, &subject::f3, {{"arg1", 0}, {"arg2", 0}});
+//    Engine engine;
+//    engine.register_command(&wrapper, "command1");
+//    std::cout << engine.execte("command1", {{"arg1", 4}, {"arg2", 5}}) << std::endl;
+//
+
     return 0;
 }
